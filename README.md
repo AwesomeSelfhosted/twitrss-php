9436a3a3ab112dd81b3ce2d32b1f5dae727ae7d9
Ram <tekkiram@gmail.com>
2017-09-02 17:07:35 +0300

    working version

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# twitrss-php
+version 1.0
--- index.php
+++ index.php
@@ -17,24 +17,62 @@ function check_user($user){
     curl_close($curl);  
     return  $user_status;  
 }
-$user = $_GET['user'];
-$hashtag = $_GET['hashtag'];
-if ($hashtag){
-    $rss_page = file_get_contents(perl_hashtag_api_url."?term={$hashtag}");
-    echo $rss_page;
-}else {
-if (!$user){
-    $rand_user_list = array('nisopict_bot_kr','nisopict_bot_k2','kneehigh_bot','akogare_ryoiki','exposed_cranium');
-    $user = $rand_user_list[rand(0,count($rand_user_list) - 1)];
-    header("HTTP/1.1 301 Moved Permanently");
-    header("Location: http://tprss.puteulanus.com/index.php?user={$user}");
-    exit;
+$path = '';
+$user =  '';
+$hashtag = '';
+$keyword = '';
+$feed ='';
+if ( isset( $_GET['path'] ) && !empty( $_GET['path'] ) ) {
+   $path = $_GET['path'];//twitrss.org/user or twitrss.org/hashtag/tag
 }
-if (!check_user($user)){
+if ( isset( $_GET['user'] ) && !empty( $_GET['user'] ) ) {
+$user = $_GET['user'];//search?user=
+}
+if ( isset( $_GET['hashtag'] ) && !empty( $_GET['hashtag'] ) ) {
+$hashtag = $_GET['hashtag'];//search?hashtag=
+}
+if ( isset( $_GET['q'] ) && !empty( $_GET['q'] ) ) {
+$keyword = $_GET['q'];//search?q=gamdeals
+}
+if ( isset( $_GET['feed'] ) && !empty( $_GET['feed'] ) ) {
+$feed = $_GET['feed'];//search?q=gamdeals
+}
+if (!empty($path))
+{
+// format of path /user/feed, hashtag/gamedeals/feed
+    $temp = explode( "/", trim($path) );
+    if ($temp[0] == "hashtag") {
+        $hashtag = $temp[1];
+        if ($temp[2] == "feed") {
+            $feed = 1;
+        }
+    } else {
+        $user = $temp[0];
+        if ($temp[1] == "feed") {
+             $feed = 1;
+        }
+    }
+}
+if(!empty($hashtag)){
+    $rss_page = file_get_contents(perl_hashtag_api_url."?term={$hashtag}");
+    if (empty($feed)) {
+        header("Content-Type: text/html; charset=utf-8");
+	echo "feed";
+    }
+    echo $rss_page;
+}else if (!empty($user)){
+    if (!check_user($user)){
+        header("Content-Type: text/html; charset=utf-8");
+        echo "not found";
+        exit;
+    }
+    $rss_page = file_get_contents(perl_api_url."?user={$user}");
+    if (empty($feed)) {
+        header("Content-Type: text/html; charset=utf-8");
+	echo "feed";
+    }
+    echo $rss_page;
+} else {
     header("Content-Type: text/html; charset=utf-8");
     echo "not found";
-    exit;
-}
-$rss_page = file_get_contents(perl_api_url."?user={$user}");
-echo $rss_page;
 }
